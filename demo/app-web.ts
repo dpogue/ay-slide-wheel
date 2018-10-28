0b5ea1dfde41a743ffdaa0df79591ee33e9314c1
Ian Suda <ian@ayogo.com>
2018-10-28 11:09:01 -0700

    Web component working!

--- demo/app-web.ts
+++ demo/app-web.ts
@@ -1,1 +1,11 @@
 import '../src/web-component';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const elem = document.querySelector('ay-slide-wheel');
+  const update = document.querySelector('#currentVal');
+  update.textContent = "0";
+  elem.addEventListener('onChange', (evt: CustomEvent) => {
+    update.textContent = evt.detail.value.toString();
+  });
+});
+
--- src/web-component/index.ts
+++ src/web-component/index.ts
@@ -10,15 +10,15 @@ export class AySlideWheel extends HTMLElement {
   connectedCallback() {
     new SlideWheel(
       this,
-      function(val) {
-        console.log("new Value:", val);
+      (val) => {
+        this.dispatchEvent(new CustomEvent('onChange', {detail: val}));
       },
       parseInt(this.getAttribute('value'), 10),
       this.getAttribute('enabled') === 'false' ? false : true,
       parseInt(this.getAttribute('min'), 10),
       parseInt(this.getAttribute('max'), 10),
       this.getAttribute('rounded-line') === 'false' ? false : true,
-      parseInt(this.getAttribute('line-thickness'), 10),
+      parseFloat(this.getAttribute('line-thickness')),
       this.getAttribute('line-start-colour'),
       this.getAttribute('line-mid-colour'),
       this.getAttribute('line-end-colour')
